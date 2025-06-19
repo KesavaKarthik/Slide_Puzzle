93aa02ecd92cf0e940d91fdf55b9fe7fb69a2061
Kesavakarthik <you@example.com>
2025-06-19 21:27:02 +0530

    Implement A* algorithm with Manhattan heuristic for 15-puzzle solver and added solvability check for initial puzzle state

--- puzzleCode.cpp
+++ puzzleCode.cpp
@@ -1,17 +1,29 @@
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <queue>
 #include <map>
 #include <set>
-//#define int long long
 using namespace std;
+
+int Manhatten_distance(vector<vector<int>> input ){
+    int distance = 0;
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if( input[i][j] != 0 )distance += abs( (input[i][j] - 1)/ 4 - i ) + abs( (input[i][j] - 1) % 4 - j);
+        }
+    }
+    return distance;
+
+}
 
 class Puzzle{
 
 private :
     vector<vector<int>> board;
+    int depth;
+
+
 
 public :
     Puzzle(){}
@@ -30,8 +42,13 @@ public :
         }
     }
 
-    void generatePath( queue <Puzzle> &boards , set<vector<vector<int>>> &vis){
-        int x , y , temp;
+    bool operator<(const Puzzle& other) const {
+        return board < other.board;
+    }
+
+
+    void generatePath( priority_queue<tuple<int, int, Puzzle>, vector<tuple<int, int, Puzzle>>, greater<tuple<int, int, Puzzle>>> &boards, set<vector<vector<int>>> &vis , int g ){
+        int x , y , temp , h;
         vector<vector<int>> new_board;
         for(int i = 0; i < 4; i++){
             for(int j = 0 ; j < 4; j++){
@@ -48,7 +65,8 @@ public :
             new_board[x - 1][y] = 0;
             new_board[x][y] = temp;
             if( vis.find(new_board) == vis.end() ){
-                boards.push(Puzzle(new_board));
+                h = Manhatten_distance(new_board);
+                boards.push({g + h + 1 , g + 1 , Puzzle(new_board)} );
                 vis.insert(new_board);
             }
             
@@ -61,7 +79,8 @@ public :
             new_board[x + 1][y] = 0;
             new_board[x][y] = temp;
             if( vis.find(new_board) == vis.end() ){
-                boards.push(Puzzle(new_board));
+                h = Manhatten_distance(new_board);
+                boards.push({g + h + 1, g + 1 , Puzzle(new_board)} );
                 vis.insert(new_board);
             }
             
@@ -74,7 +93,8 @@ public :
             new_board[x][y - 1] = 0;
             new_board[x][y] = temp;
             if( vis.find(new_board) == vis.end() ){
-                boards.push(Puzzle(new_board));
+                h = Manhatten_distance(new_board);
+                boards.push({g + h + 1 , g + 1 , Puzzle(new_board)} );
                 vis.insert(new_board);
             }
 
@@ -85,7 +105,8 @@ public :
             new_board[x][y + 1] = 0;
             new_board[x][y] = temp;
             if( vis.find(new_board) == vis.end() ){
-                boards.push(Puzzle(new_board));
+                h = Manhatten_distance(new_board);
+                boards.push({g + h + 1 , g + 1 , Puzzle(new_board)} );
                 vis.insert(new_board);
             }
             
@@ -98,6 +119,65 @@ public :
    
     
 };
+int merge_sort(vector<int> &num){
+    int size = num.size();
+    if(size == 1){
+        return 0;
+    }
+    int mid = size/ 2;
+    vector<int> left (num.begin() , num.begin() + mid);
+    vector<int> right ( num.begin() + mid, num.end());
+
+    int lc = merge_sort(left);
+    int rc = merge_sort(right);
+    int i = 0 , j = 0;
+    vector<int> temp( num.size());
+    int t = 0;
+    int count = 0;
+    while(i < left.size() || j < right.size() ){
+        if( i >= left.size()){
+            temp[t] = right[j];
+            j++;
+        }
+        else if( j >= right.size()){
+            temp[t] = left[i];
+            i++;
+        }
+        else if( left[i] < right[j]){
+            temp[t] = left[i];
+            i++;
+        }
+        else{
+            count += left.size() - i;
+            temp[t] = right[j];
+            j++;
+        }
+        t++;
+    }
+    num = temp ;
+    return count + lc + rc;
+}
+
+
+bool solvability_check(vector<vector<int>> input){
+    vector<int> num(15);
+    int row_number;
+    int t = 0;
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if( input[i][j] != 0 ){
+                num[t] = input[i][j];
+                t++;
+            }
+            else row_number = 4 - i;
+        }
+    }
+    int inversions = merge_sort(num);
+    if( (inversions + row_number) % 2 == 0) return false;
+    else return true;
+}
+
+
 int main(){
     vector<vector<int>> input( 4, vector<int>(4 , 0));
     set<vector<vector<int>>> vis;
@@ -108,34 +188,37 @@ int main(){
         }
     }
     vis.insert(input);
+    bool check = solvability_check(input);
+    if( check == false){
+        cout << "Incorrect input " << endl;
+        return 0;
+    }
     vector<vector<int>> ans;
     ans = {{1 , 2, 3, 4},
            {5 , 6, 7, 8},
            {9 , 10 , 11, 12},
            {13 , 14 , 15 , 0}};
-
-    queue<Puzzle> boards;
-    boards.push(Puzzle(input));
+    
+    int distance = Manhatten_distance(input);
+    priority_queue<tuple<int, int, Puzzle>, vector<tuple<int, int, Puzzle>>, greater<tuple<int, int, Puzzle>>> boards;
+    boards.push({ distance , 0  ,Puzzle(input)});
     Puzzle board;
+    int g , f;
     while(!boards.empty()){
-        board = boards.front();
+        f = get<0>(boards.top());
+        g = get<1>(boards.top());
+        board = get<2>(boards.top());
+        //board.print();
+        //cout << " "<< endl;
         boards.pop();
         if( board.display() == ans){
             cout << "found " << endl;
-            board.print();
-            
+            cout << "Count = " << g << endl;
             break;
         }
-        board.generatePath( boards , vis);
 
-
-
-
-
-
-
-
-
+        board.generatePath( boards , vis , g );
+        
 
     }
     return 0;
